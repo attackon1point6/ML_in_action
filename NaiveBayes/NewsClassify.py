3c036300399b29eb067b53e9190dc0a01782653b
attack1point6 <1293767307@qq.com>
2022-08-10 00:50:02 +0800

    贝叶斯分类文档

---
+++ NaiveBayes/NewsClassify.py
@@ -0,0 +1,184 @@
+from sklearn.naive_bayes import MultinomialNB
+import matplotlib.pyplot as plt
+import os
+import random
+import jieba
+
+"""
+函数说明：中文文本处理
+
+Parameters:
+    folder_path:文本存放路径
+    test_size:测试集占比，默认占所有数据集的百分之20
+Returns:
+    all_words_list:按词频降序排序的训练集列表
+    train_data_list:训练集列表
+    test_data_list:测试集列表
+    train_class_list:训练集标签列表
+    test_class_list:测试集标签列表
+Author:
+    Jia Zechen
+Time:
+    2022/08/09
+"""
+def TextProcessing(folder_path, test_size = 0.2):
+    folder_list = os.listdir(folder_path)
+    data_list = []
+    class_list = []
+
+    # 遍历每个子文件列表
+    for folder in folder_list:
+        new_folder_path = os.path.join(folder_path, folder)
+        files = os.listdir(new_folder_path)
+
+        j = 1
+        # 遍历当前文件夹下每个txt文件
+        for file in files:
+            if j > 100:
+                break # 每类txt最多取样100个
+            with open(os.path.join(new_folder_path, file), 'r', encoding = 'utf-8') as f:
+                raw = f.read()
+            word_cut = jieba.cut(raw, cut_all = False)
+            word_list = list(word_cut)
+
+            data_list.append(word_list)
+            class_list.append(folder)
+            j += 1
+
+    data_class_list = list(zip(data_list, class_list))
+    random.shuffle(data_class_list)
+    index = int(len(data_class_list) * test_size) + 1  # 训练集和测试集切分的索引值
+    train_list = data_class_list[index:]  # 训练集
+    test_list = data_class_list[:index]  # 测试集
+    train_data_list, train_class_list = zip(*train_list)  # 训练集解压缩
+    test_data_list, test_class_list = zip(*test_list)  # 测试集解压缩
+
+    all_words_dict = {}
+    for word_list in train_data_list:
+        for word in word_list:
+            if word in all_words_dict:
+                all_words_dict[word] += 1
+            else:
+                all_words_dict[word] = 1
+
+    # 根据键的值倒序排序
+    all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f: f[1], reverse=True)
+    all_words_list, all_words_nums = zip(*all_words_tuple_list)  # 解压缩
+    all_words_list = list(all_words_list)  # 转换成列表
+    return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
+
+"""
+函数说明：读取文件里的内容，并去重
+
+Parameters:
+    words_file:文件路径
+Returns:
+    words_set:读取的内容的set集合
+Author:
+    Jia Zechen
+Time:
+    2022/08/09
+"""
+def MakeWordsSet(words_file):
+    words_set = set()
+    with open(words_file, 'r', encoding = 'utf-8') as f:
+        for line in f.readlines():
+            word = line.strip()
+            if len(word) > 0:
+                words_set.add(word)
+    return words_set
+
+"""
+函数说明:根据feature_words将文本向量化
+
+Parameters:
+    train_data_list:训练集
+    test_data_list:测试集
+    feature_words:特征集
+Returns:
+    train_feature_list:训练集向量化列表
+    test_feature_list:测试集向量化列表
+Author:
+    Jia Zechen
+Time:
+    2022/08/09
+"""
+def TextFeatures(train_data_list, test_data_list, feature_words):
+    def text_features(text, feature_words):
+        text_words = set(text)
+        features = [1 if word in text_words else 0 for word in feature_words]
+        return features
+    train_features_list = [text_features(text, feature_words) for text in train_data_list]
+    test_features_list = [text_features(text, feature_words) for text in test_data_list]
+    return train_features_list, test_features_list
+
+"""
+函数说明:文本特征选取
+
+Parameters:
+    all_words_list:训练集所有文本列表
+    deleteN:删除词频最高的deleteN个词
+    stopwords_set:指定的结束语
+Returns:
+    feature_words:特征集
+Author:
+    Jia Zechen
+Time:
+    2022/08/09
+"""
+def words_dict(all_words_list, deleteN, stopwords_set = set()):
+    feature_words = []
+    n = 1
+    for t in range(deleteN, len(all_words_list), 1):
+        if n > 1000:
+            break
+        # 如果这个词不是数字，并且不是指定的结束语，并且单词长度大于1小于5，那么这个词就可以作为特征词
+        if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(all_words_list[t]) < 5:
+            feature_words.append(all_words_list[t])
+        n += 1
+    return feature_words
+
+
+"""
+函数说明:新闻分类器
+
+Parameters:
+    train_feature_list:训练集向量化的特征文本
+    test_feature_list:测试集向量化的特征文本
+    train_class_list:训练集分类标签
+    test_class_list:测试集分类标签
+Returns:
+    test_accuracy:分类器精度
+Author:
+    Jia Zechen
+Time:
+    2022/08/09
+"""
+def TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list):
+    classifier = MultinomialNB().fit(train_feature_list, train_class_list)
+    test_accuracy = classifier.score(test_feature_list, test_class_list)
+    return test_accuracy
+
+if __name__ == '__main__':
+    # 文本预处理
+    folder_path = './SogouC/Sample'  # 训练集存放地址
+    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path,
+                                                                                                        test_size=0.2)
+    # 生成stopwords_set
+    stopwords_file = './stopwords_cn.txt'
+    stopwords_set = MakeWordsSet(stopwords_file)
+
+    test_accuracy_list = []
+    deleteNs = range(0, 1000, 20)
+    for deleteN in deleteNs:
+        feature_words = words_dict(all_words_list, deleteN, stopwords_set)
+        train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
+        test_accuracy = TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list)
+        test_accuracy_list.append(test_accuracy)
+
+    plt.figure()
+    plt.plot(deleteNs, test_accuracy_list)
+    plt.title('Relationship of deleteNs and test_accuracy')
+    plt.xlabel('deleteNs')
+    plt.ylabel('test accuracy')
+    plt.show()
